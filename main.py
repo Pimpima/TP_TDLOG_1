bceef1ad037d32ec2ea5569cfc41802af76b2316
Joy-Rose Dunoyer <joy-rose.dunoyer-de-segonzac@eleves.enpc.fr>
2021-09-27 15:45:20 +0200

    comments

--- main.py
+++ main.py
@@ -2,21 +2,28 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
 
-
-class Tile():
-    def __init__(self, bool #bool, tile_type #int, tile_orientation #int):
-        Tile._static = bool
+class Tile:
+    '''Tile class : corresponds to a piece of the game with several exits
+    (2,3 or 4) that can rotate or move if it is not static'''
+    def __init__(self, tile_bool:bool = False, tile_type:int = 0,tile_orientation:int = 0):
+        '''The tile is initialized by being static or dynamic (bool), by choosing a type of tile
+        (there are 3 different types) and a rotation of the tile ; we have chosen a reference side of each tile.'''
+        Tile._static = tile_bool
         Tile.type = tile_type
         Tile.orientation = tile_orientation
     def rotate(self):
+        '''This function rotates the tile of 90° clockwise'''
         Tile.orientation = (Tile.orientation +1)%4
+
+
+class Board:
 
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
     T = Tile(True, 0, 0)
-    print_hi('PyCharm')
+
