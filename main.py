ea26b2f47988f5277ecb469d1eae38a0ad0bf9b6
Joy-Rose Dunoyer <joy-rose.dunoyer-de-segonzac@eleves.enpc.fr>
2021-09-27 15:16:08 +0200

    tp1

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-### ---------------- TP - Labyrinthe ---------------- ###
-### Joy-Rose DUNOYER DE SEGONZAC & Pierre-Marie CIRON ###
+# This is a sample Python script.
 
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+class Tile():
+    def __init__(self, bool #bool, tile_type #int, tile_orientation #int):
+        Tile._static = bool
+        Tile.type = tile_type
+        Tile.orientation = tile_orientation
+    def rotate(self):
+        Tile.orientation = (Tile.orientation +1)%4
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    T = Tile(True, 0, 0)
+    print_hi('PyCharm')
