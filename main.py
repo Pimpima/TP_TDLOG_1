718ecaadb73aef62fbf1e000bdf53ea2c4e6345a
Pierre-Marie CIRON <pierre-marie.ciron@enpc.fr>
2021-09-27 15:41:55 +0200

    Object

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-class Tile():
-    def __init__(self, bool #bool, tile_type #int, tile_orientation #int):
+class Tile:
+    def __init__(self, bool '''bool''', tile_type #int, tile_orientation #int ):
         Tile._static = bool
         Tile.type = tile_type
         Tile.orientation = tile_orientation
     def rotate(self):
         Tile.orientation = (Tile.orientation +1)%4
 
-
+class Object:
+    object_list=["book", "emerald", "gold", "key ring", "map", "owl", "ring", "sword"]
+    def __init__(self, object_type=0 '''int in {0,..,7}'''):
+        Object.object=object_list[object_type]
 
 
 # Press the green button in the gutter to run the script.
@@ -20,3 +19,4 @@ if __name__ == '__main__':
 
     T = Tile(True, 0, 0)
     print_hi('PyCharm')
+
